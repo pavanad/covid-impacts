57402df2ace8fbfb82195ab7190bd4f7ea9535aa
Adilson Pavan <adilson.pavan@gmail.com>
2020-06-27 11:40:39 -0300

    Init collector project

--- collector/__main__.py
+++ collector/__main__.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def main():
-    pass
+import timeit
 
+from services.extract import ExtractService
+
+
+def main():
+    start = timeit.default_timer()
+    extract = ExtractService()
+    extract.execute()
+    timelapse = timeit.default_timer() - start
+
+    print(f"timelapse: {timelapse}")
+    
 if __name__ == "__main__":
     main()
